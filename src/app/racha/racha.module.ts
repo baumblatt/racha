fd22efcb3aaaff4290e4fc8c82ecdd90244df59b
baumblatt <baumblatt@gmail.com>
2018-05-07 09:16:44 -0300

    NGRX Store for Racha feature module

--- src/app/racha/racha.module.ts
+++ src/app/racha/racha.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './containers/home/home.component';
 
 import { RachaRoutingModule } from './racha-routing.module';
 
 @NgModule({
-	imports: [SharedModule, RachaRoutingModule],
+	imports: [SharedModule, EffectsModule.forFeature([]), StoreModule.forFeature('racha', {}), RachaRoutingModule],
 	declarations: [HomeComponent]
 })
 export class RachaModule {}
