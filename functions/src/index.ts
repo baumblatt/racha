ba4763b71e8b773dfea65d8175b23753c8284a16
Bernardo Baumblatt <baumblatt@gmail.com>
2018-05-25 00:55:24 -0300

    Cloud Functions to create player on create user.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+import * as  admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
+
+admin.initializeApp(functions.config().firebase);
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+export const createJogador = functions.auth.user().onCreate(function(event) {
+	const { uid, displayName, photoURL, email } = event;
+
+	// Get the uid and display name of the newly created user
+	return admin.firestore().doc(`/jogadores/${uid}`).set({ uid, displayName, photoURL, email, role: 'guest' });
 });
