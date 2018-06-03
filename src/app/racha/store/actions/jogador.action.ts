f283be93b1ce15b0f85a6c026b685f23ad0008eb
Bernardo Baumblatt <baumblatt@gmail.com>
2018-06-03 20:47:55 -0300

    Refactoring of players actions.

--- src/app/racha/store/actions/jogador.action.ts
+++ src/app/racha/store/actions/jogador.action.ts
@@ -1,32 +1,32 @@
 import { Action } from '@ngrx/store';
 import { Jogador } from '../../models/jogador.model';
 
-export const LOAD_JOGADORES = '[Jogadores] Load Jogadores.';
-export const LOAD_JOGADORES_SUCCESS = '[Jogadores] Load Jogadores (Success).';
-export const LOAD_JOGADORES_FAIL = '[Jogadores] Load Jogadores (Fail).';
-export const LOAD_JOGADORES_STOP = '[Jogadores] Load Jogadores (Stop)';
+export const OBSERVE_JOGADORES_SUBSCRIBE = '[Jogadores] Observe Jogadores (Subscribe).';
+export const OBSERVE_JOGADORES_NEXT = '[Jogadores] Observe Jogadores (Next).';
+export const OBSERVE_JOGADORES_ERROR = '[Jogadores] Observe Jogadores (Error).';
+export const OBSERVE_JOGADORES_UNSUBSCRIBE = '[Jogadores] Observe Jogadores (Unsubscribe).';
 
-export class LoadJogadores implements Action {
-	readonly type = LOAD_JOGADORES;
+export class ObserveJogadoresSubscribe implements Action {
+	readonly type = OBSERVE_JOGADORES_SUBSCRIBE;
 }
 
-export class LoadJogadoresSuccess implements Action {
-	readonly type = LOAD_JOGADORES_SUCCESS;
+export class ObserveJogadoresNext implements Action {
+	readonly type = OBSERVE_JOGADORES_NEXT;
 
 	constructor(public payload: Jogador[]) {
 	}
 }
 
-export class LoadJogadoresFail implements Action {
-	readonly type = LOAD_JOGADORES_FAIL;
+export class ObserveJogadoresError implements Action {
+	readonly type = OBSERVE_JOGADORES_ERROR;
 
 	constructor(public payload: any) {
 	}
 }
 
-export class LoadJogadoresStop implements Action {
-	readonly type = LOAD_JOGADORES_STOP;
+export class ObserveJogadoresUnsubscribe implements Action {
+	readonly type = OBSERVE_JOGADORES_UNSUBSCRIBE;
 }
 
 
-export type JogadorAction = LoadJogadores | LoadJogadoresSuccess | LoadJogadoresFail | LoadJogadoresStop;
+export type JogadorAction = | ObserveJogadoresSubscribe | ObserveJogadoresNext | ObserveJogadoresError | ObserveJogadoresUnsubscribe ;
